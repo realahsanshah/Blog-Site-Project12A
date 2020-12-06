deb529ef78eb2d552578e8f04c955f26655b29c4
Ahsan Shah <56998278+realsashah@users.noreply.github.com>
2020-12-06 19:03:40 +0500

    added env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,15 @@
 const dotenv=require('dotenv');
 
 dotenv.config();
+let spaceIdValue;
+let accessTokenValue
+if (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') {
+    spaceIdValue=process.env.CONTENTFUL_SPACE_ID;
+    accessTokenValue=process.env.CONTENTFUL_ACCESS_TOKEN
+} else {
+    spaceIdValue=secrets.CONTENTFUL_SPACE_ID;
+    accessTokenValue=secrets.CONTENTFUL_ACCESS_TOKEN
+}
 
 module.exports = {
     plugins: [
@@ -9,8 +18,8 @@ module.exports = {
         {
             resolve: `gatsby-source-contentful`,
             options: {
-                spaceId:process.env.CONTENTFUL_SPACE_ID,
-                accessToken:process.env.CONTENTFUL_ACCESS_TOKEN
+                spaceId:spaceIdValue,
+                accessToken:accessTokenValue
             },
           },
     ]
